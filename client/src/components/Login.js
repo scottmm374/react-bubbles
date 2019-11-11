009b4af0d8d96a290626c54dadc9674a3ebf200c
Michelle Scott <scottmm374@gmail.com>
2019-11-10 19:29:32 -0500

    Login form working and getting token

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,25 +6,44 @@ const Login = () => {
     username: "",
     password: ""
   });
+
+  const handleChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    api()
+      .post("/api/login", user)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        console.log("login success");
+        // props.history.push("/");
+      })
+      .catch(err => {
+        console.log("login error", err.data.message);
+      });
+  };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="username"
-          value=""
+          value={user.username}
           placeholder="username"
-          onChange=""
+          onChange={handleChange}
         />
         <input
           type="password"
           name="password"
-          value=""
+          value={user.passwor}
           placeholder="password"
-          onChange=""
+          onChange={handleChange}
         />
         <button type="submit">Login</button>
       </form>
